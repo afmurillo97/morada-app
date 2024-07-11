8b0f61ceac24818c92dd70a1194f6903dc2114eb
afmurillo97 <afmurillo97@gmail.com>
2024-07-11 18:12:42 -0500

    Added routes for Google OAuth 2.0 authentication using GoogleController for redirection and callback handling.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\GoogleController;
 
+/** 
+ * Public Routes in Morada APP
+ */
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('auth.google');
+Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+
+/** 
+ * Private Routes in Morada APP
+ */
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
