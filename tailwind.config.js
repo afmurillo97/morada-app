c6a2849fc840e37e9c35bc0286d22ef8c7c23c54
afmurillo97 <afmurillo97@gmail.com>
2024-07-11 13:39:29 -0500

    install flowbite dependency and enable profile proto in jetstream.php

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './node_modules/flowbite/**/*.js'
     ],
 
     theme: {
@@ -19,5 +22,9 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [
+        forms, 
+        typography,
+        require('flowbite/plugin')
+    ],
 };
