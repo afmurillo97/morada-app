994a4fbe396eabd7b9da423a30974fb09e5bcca9
afmurillo97 <afmurillo97@gmail.com>
2024-07-11 18:13:16 -0500

    Initial setup for unit testing User model within Tests\Unit\Models directory.

---
+++ tests/Unit/Models/UserTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use Illuminate\Database\Eloquent\Collection;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
