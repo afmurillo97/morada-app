a7ea0ce430a7520b37c8cab80ed63293b1f60f5f
afmurillo97 <afmurillo97@gmail.com>
2024-07-11 18:08:05 -0500

    Add Google OAuth login functionality with user registration and profile photo update.

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+
+class GoogleController extends Controller
+{
+
+    public function redirectToGoogle() 
+    {
+        return Socialite::driver('google')->redirect();    
+    }
+
+    public function handleGoogleCallback() 
+    {
+        $user = Socialite::driver('google')->stateless()->user();
+        $this->_registerOrLoginUser($user);
+        return redirect()->intended('dashboard');
+    }
+
+    public function _registerOrLoginUser($data) 
+    {
+        $user = User::where('email', $data->email)->first();
+
+        if(!$user){
+            $user = new User();
+            $user->name = $data->name;
+            $user->role_id = 2;
+            $user->email = $data->email;
+            $user->email_verified_at = now();
+            $user->provider_id = $data->id;
+            $user->password = Str::random(16); // Generate a random password
+            $user->status = '1';
+            $user->profile_photo_path = $data->avatar;
+            
+            $user->save();
+        } else {
+            $update = false;
+    
+            if (!empty($data->id)) {
+                $user->provider_id = $data->id;
+                $update = true;
+            }
+    
+            if (!empty($data->avatar)) {
+                $user->profile_photo_path = $data->avatar;
+                $update = true;
+            }
+    
+            if ($update) {
+                $user->save();
+            }
+        }
+
+        Auth::login($user);
+    }
+
+}
