24cb8e92bb76c05996951979e011fd9e380b8f71
afmurillo97 <afmurillo97@gmail.com>
2024-07-12 17:33:46 -0500

    include role and other features in register controller

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -3,6 +3,8 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
@@ -26,10 +28,21 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
+        $role = Role::firstOrCreate(['id' => 2], ['name' => 'USER']);
+
         return User::create([
             'name' => $input['name'],
+            'role_id' => $role->id,
             'email' => $input['email'],
+            'email_verified_at' => now(),
+            'provider_id' => null,
             'password' => Hash::make($input['password']),
+            'status' => '1',
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
         ]);
     }
 }
